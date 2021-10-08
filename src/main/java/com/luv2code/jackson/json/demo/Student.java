9b71e97eec76a5761c74477bdbaacd380a506a2b
salmanasad123 <slman.me@gmail.com>
2021-10-08 11:35:33 +0500

    @JsonIgnoreProperties(ignoreUnknown = true) annotation added

--- src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -1,6 +1,11 @@
 package com.luv2code.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 // creating student pojo class
+// this annotation will ignore unknown properties that we don't know, or we didn't map in our pojo
+// it will ignore it and will not cause exception
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
     private int id;
